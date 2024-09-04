9f7c4c6ecddd10e27c854ed4703f25a405eb6ee0
dumichanda <33031037+dumichanda@users.noreply.github.com>
2024-09-04 22:59:35 +0200

    Update Image.jsx

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,9 +1,11 @@
 export default function Image({src,...rest}) {
   src = src && src.includes('https://')
-    ? src
-    : 'http://https://airbnb-clone-ih0r7gqbi-dumichandas-projects.vercel.app
-/uploads/'+src;
+  ? src
+  : 'http://airbnb-clone-ih0r7gqbi-dumichandas-projects.vercel.app/uploads/' + src; 
   return (
     <img {...rest} src={src} alt={''} />
   );
 }
+src = src && src.includes('https://')
+  ? src
+  : 'http://airbnb-clone-ih0r7gqbi-dumichandas-projects.vercel.app/uploads/' + src;
